f1d195868baf91e1d9f9e91799eedf23e47806d4
Michael Murphy <murphym1219@gmail.com>
2022-01-25 11:51:21 -0800

    Updates, refining, changes

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ac99cd78d7156456fb854a08265af0e7-217817222.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
